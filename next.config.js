928bdd5cfb9e9f16e48022062012dd33011f09bd
Karaay <43575365+karaaykaraogul@users.noreply.github.com>
2025-03-09 12:54:50 +0300

    next config change

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export",
+  // Add any other configuration options here
+  // If you need to handle images properly in static export:
+  images: {
+    unoptimized: true,
+  },
 };
 
-module.exports = {
-  output: "export",
-};
+module.exports = nextConfig;
